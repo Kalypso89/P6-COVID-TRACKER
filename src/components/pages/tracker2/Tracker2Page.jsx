08acfbe4525f9b42fd563793e6eabdcb4c141b8d
Angela <antunezangela89@gmail.com>
2023-12-21 13:48:09 +0100

    Correct mistake in Tracker 2 and 3

--- src/components/pages/tracker2/Tracker2Page.jsx
+++ src/components/pages/tracker2/Tracker2Page.jsx
@@ -2,7 +2,7 @@ import { Tracker0Organism } from "../../organism/tracker0/Tracker0Organism";
 export function Tracker2Page() {
   return (
     <>
-      <h1>Éste es el componente Tracker1</h1>
+      <h1>Éste es el componente Tracker2</h1>
       <Tracker0Organism></Tracker0Organism>
     </>
   );
--- src/components/pages/tracker3/Tracker3Page.jsx
+++ src/components/pages/tracker3/Tracker3Page.jsx
@@ -2,7 +2,7 @@ import { Tracker0Organism } from "../../organism/tracker0/Tracker0Organism";
 export function Tracker3Page() {
   return (
     <>
-      <h1>Éste es el componente Tracker1</h1>
+      <h1>Éste es el componente Tracker3</h1>
       <Tracker0Organism></Tracker0Organism>
     </>
   );
