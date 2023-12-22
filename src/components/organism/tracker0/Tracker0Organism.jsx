27a1340d2d992e726fe53d938f3e01976250b197
Angela <antunezangela89@gmail.com>
2023-12-23 00:04:16 +0100

    Finish Tracker0 structure

--- src/components/organism/tracker0/Tracker0Organism.jsx
+++ src/components/organism/tracker0/Tracker0Organism.jsx
@@ -1,9 +1,10 @@
+import { TotalStatistics } from "../../molecules/totalStatistics/TotalStatistics";
+import { Top10Country } from "../../molecules/top10Country/Top10Country";
 export function Tracker0Organism() {
     return (
-      <>
-        <div>
-          
-        </div>
-      </>
+      <aside>
+        <TotalStatistics></TotalStatistics>
+        <Top10Country></Top10Country> 
+      </aside>
     );
   }
