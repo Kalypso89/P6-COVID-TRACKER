e72a863ac58bfc3b3ddb49e43d0b1d095081aa61
Angela <antunezangela89@gmail.com>
2023-12-23 00:07:24 +0100

    Finish TotalStatistics component structure (change API props later)

--- src/components/molecules/totalStatistics/TotalStatistics.jsx
+++ src/components/molecules/totalStatistics/TotalStatistics.jsx
@@ -1,41 +1,22 @@
-export function TotalStatistics() {
-    const total_statistics = useTotalStatistics();
-    return (
-      <>
-        <div>
-          <h1>COVID-19 Tracker</h1>
-          <div className="row_statistics">
-                            {
-                                total_statistics.slice(0, 3).map(({ title, variation, total }) => (
-                                    <RowStatistics 
-                                        title={titleAPI}
-                                        variation={variationAPI}
-                                        total={totalAPI}
-                                    />
-                                ))
-                            }
-          </div>
-        </div>
-      </>
-    );
-  }
+import { useTotalStatistics } from "../../../configuration/hooks/useTotalStatistics";
+import { RowStatistics } from "../../atoms/RowStatistics";
 
-  export function TestimonialsCards({opinion, user}) {
+export function TotalStatistics() {
+    const totalStatistics = useTotalStatistics();
     return (
-      <div className="testimonials__cards">
-          <div className="testimonials__score">
-              <i className="fa-solid fa-star"></i> 
-              <i className="fa-solid fa-star"></i> 
-              <i className="fa-solid fa-star"></i> 
-              <i className="fa-solid fa-star"></i> 
-              <i className="fa-solid fa-star"></i> 
-          </div>
-          <div className="testimonials__opinion">
-              <p>{opinion}</p>
-          </div>
-          <div className="testimonials__user"> 
-              <p><b>{user}</b></p>
-          </div>
-      </div>
-    )
+      <section>
+        <h1>COVID-19 Tracker</h1>
+        <div className="statistics_container">
+          {
+              totalStatistics.map(({ titleAPI, variationAPI, totalAPI }) => (
+                  <RowStatistics 
+                      title={titleAPI}
+                      variation={variationAPI}
+                      total={totalAPI}
+                  />
+              ))
+          }
+        </div>
+      </section>
+    );
   }
