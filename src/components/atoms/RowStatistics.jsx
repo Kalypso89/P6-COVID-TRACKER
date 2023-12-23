c0a9f9729bdc5972ff5941dd04c7db1414928434
Angela <antunezangela89@gmail.com>
2023-12-23 22:45:58 +0100

    Finish RowStatistics structure

--- src/components/atoms/RowStatistics.jsx
+++ src/components/atoms/RowStatistics.jsx
@@ -1,7 +1,15 @@
-export function RowStatistics({titleAPI, variationAPI, totalAPI}) {
+export function RowStatistics({title, today, total}) {
   return (
     <div className="statistics_row">
-        <div></div>
+        <div className="statistics_row_title">
+          <p>{title}</p>
+        </div>
+        <div className="statistics_row_today">
+          <p>{today}</p>
+        </div>
+        <div className="statistics_row_total">
+          <p>{total}</p>
+        </div>
     </div>
   )
 }
