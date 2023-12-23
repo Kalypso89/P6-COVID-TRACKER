76095fa48ce5a9298e6076d769eddd49e65f0c4d
Angela <antunezangela89@gmail.com>
2023-12-23 22:44:44 +0100

    Fix undefined title in TotalStatistics component

--- src/components/molecules/totalStatistics/TotalStatistics.jsx
+++ src/components/molecules/totalStatistics/TotalStatistics.jsx
@@ -10,7 +10,7 @@ export function TotalStatistics() {
           {
               totalStatistics.map((row) => (
                   <RowStatistics 
-                      key={title}
+                      key={row.title}
                       {...row}
                   />
               ))
