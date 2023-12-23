8d04b6fe934979913e76368357b8317ae73585b1
Angela <antunezangela89@gmail.com>
2023-12-23 22:20:33 +0100

    Finish hook for TotalStatistics component

---
+++ src/hooks/useTotalStatistics.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import API_URL from "config.js";
+
+export function useTotalStatistics () {
+    const [totalStatistics, setTotalStatistics] = useState([]);
+
+    useEffect(() => {
+        const myEndpoint = `${API_URL}all`;
+        fetch(myEndpoint)
+            .then(request => request.json())
+            .then(json => {
+                const dataStatistics = [
+                    {
+                        title: "Total Case",
+                        today: json.todayCases,
+                        total: json.cases
+                    },
+                    {
+                        title: "Active Case",
+                        today: null,
+                        value: json.active
+                    },
+                    {
+                        title: "Recovered Case",
+                        today: json.todayRecovered,
+                        total: json.recovered
+                    },
+                    {
+                        title: "Deaths Case",
+                        today: json.todayDeaths,
+                        total: json.deaths
+                    }
+                ];
+                setTotalStatistics(dataStatistics);
+            })
+    }, [setTotalStatistics]);
+
+    return totalStatistics;
+}
