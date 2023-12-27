9456a5382b745124aeafe1c3c65ca390b9d25ecf
Desiree <desireesanchezdev@gmail.com>
2023-12-27 16:53:49 +0100

    Edit link import

--- src/components/pages/tracker1/Tracker1Page.jsx
+++ src/components/pages/tracker1/Tracker1Page.jsx
@@ -1,5 +1,5 @@
 import { Tracker0Organism } from "../../organism/tracker0/Tracker0Organism";
-import { NavbarMolecule } from "../../molecules/NavbarMolecule";
+import { NavbarMolecule } from "../../molecules/navbar/NavbarMolecule";
 
 export function Tracker1Page() {
     return (
