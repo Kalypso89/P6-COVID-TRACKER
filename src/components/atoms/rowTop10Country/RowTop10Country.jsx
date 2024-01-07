a13400604f9fc223e96762b27973b91a840ab4c8
Angela <antunezangela89@gmail.com>
2024-01-07 21:24:30 +0100

    Styles for RowTop10Country atom

--- src/components/atoms/rowTop10Country/RowTop10Country.jsx
+++ src/components/atoms/rowTop10Country/RowTop10Country.jsx
@@ -2,16 +2,10 @@ import "./rowTop10Country.css"
 
 export function RowTop10Country({flag, country, cases}) {
   return (
-    <div className="top10_row">
-        <div className="top10_row_flag">
-          <img src={flag}/>
-        </div>
-        <div className="top10_row_country">
-          <p>{country}</p>
-        </div>
-        <div className="top10_row_cases">
-          <p>{cases}</p>
-        </div>
-    </div>
+    <li className="top10_row">
+        <img src={flag} className="top10_row_flag"/>
+        <h6 className="top10_row_country">{country}</h6>
+        <span className="top10_row_cases">{cases}</span>
+    </li>
   )
 }
