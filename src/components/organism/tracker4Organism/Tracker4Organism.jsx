c3414f70061799c25fe5c4ce101d16734c8ca3f1
Angela <antunezangela89@gmail.com>
2024-01-07 19:57:42 +0100

    Add tracker 4, for testing purposes

---
+++ src/components/organism/tracker4Organism/Tracker4Organism.jsx
@@ -0,0 +1,5 @@
+export function Tracker4Organism() {
+  return (
+    <div>Tracker4Organism</div>
+  )
+}
---
+++ src/components/pages/tracker4/Tracker4Page.jsx
@@ -0,0 +1,12 @@
+import { NavbarMolecule } from "../../molecules/navbar/NavbarMolecule";
+import { Tracker0Organism } from "../../organism/tracker0Organism/Tracker0Organism";
+import { Tracker4Organism } from "../../organism/tracker4Organism/Tracker4Organism"
+
+export function Tracker4Page() {
+    return (
+      <>
+        <NavbarMolecule></NavbarMolecule>
+        <Tracker0Organism></Tracker0Organism>
+      </>
+    );
+  }
