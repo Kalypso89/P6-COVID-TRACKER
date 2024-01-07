efd1f9c8ed1e9f2f9c4b7e6389e0a2b7c85b1259
Angela <antunezangela89@gmail.com>
2024-01-07 19:48:52 +0100

    Finish Top 10 Country molecule

--- src/components/molecules/top10Country/Top10Country.jsx
+++ src/components/molecules/top10Country/Top10Country.jsx
@@ -1,5 +1,21 @@
+import { useTop10Country } from "../../../hooks/useTop10Country";
+import { RowTop10Country } from "../../atoms/rowTop10Country/RowTop10Country";
+import "./top10Country.css"
+
 export function Top10Country() {
+  const top10Country = useTop10Country();
   return (
-    <div>Top10Country</div>
+    <section>
+      <div className="top10_container">
+        {
+            top10Country.map((row) => (
+                <RowTop10Country 
+                    key={row.country}
+                    {...row}
+                />
+            ))
+        }
+      </div>
+    </section>
   )
 }
