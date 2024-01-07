8ecb3b3918967a837f021e4b19a2dcc7a297d2a0
Angela <antunezangela89@gmail.com>
2024-01-07 19:55:19 +0100

    Rename tracker0 to tracker0Organism

--- src/components/organism/tracker0Organism/Tracker0Organism.jsx
+++ src/components/organism/tracker0Organism/Tracker0Organism.jsx
@@ -1,5 +1,7 @@
 import { TotalStatistics } from "../../molecules/totalStatistics/TotalStatistics";
 import { Top10Country } from "../../molecules/top10Country/Top10Country";
+import "./tracker0Organism.css";
+
 export function Tracker0Organism() {
     return (
       <aside>
