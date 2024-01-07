0b62c60b1c97d4481d02da4d48cb828fd3052d6e
Angela <antunezangela89@gmail.com>
2024-01-07 21:52:31 +0100

    Add plus sign to today's variation in RowStatistics atom

--- src/components/atoms/rowStatistics/RowStatistics.jsx
+++ src/components/atoms/rowStatistics/RowStatistics.jsx
@@ -5,7 +5,9 @@ export function RowStatistics({title, today, total, color, backgroundColor}) {
   return (
     <li className="statistics_row" data-color={color} style={{ backgroundColor, borderColor: color }}>
         <h6 className="statistics_row_title">{title}</h6>
-        <span className="statistics_row_today">{today}</span>
+        {
+          today && <span className="statistics_row_today">+{today}</span>
+        }
         <span className="statistics_row_total" style={{ color }}>{total}</span>
     </li>
   )
