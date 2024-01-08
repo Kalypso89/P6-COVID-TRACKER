7968a05adf682b1d5fdf2243a6227f0e102d3909
Desiree <desireesanchezdev@gmail.com>
2024-01-09 00:57:57 +0100

    Create CardTotalStatisticsAtom for Tracker1 and add styles

---
+++ src/components/atoms/cardTotalStatisticsTracker1/CardTotalStatisticsTracker1Atom.jsx
@@ -0,0 +1,17 @@
+import "./cardTotalStatisticsTracker1Atom.css"
+
+export const CardTotalStatisticsTracker1Atom = () => {
+    return (
+        <div>
+            <div className="covid_total_info_widget">
+                <div className="total_covid_icon">
+                    <img src="./src/assets/icons/covid-defult.svg" alt="Covid Icon" />
+                </div>
+                <div className="total_stats_info">
+                    <h4>Total Confirmed</h4>
+                    <h3>54054</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
