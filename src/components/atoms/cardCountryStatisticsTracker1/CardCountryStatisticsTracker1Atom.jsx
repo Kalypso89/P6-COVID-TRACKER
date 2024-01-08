472805ae84977ccca3ffdebbe496187b461c3af3
Desiree <desireesanchezdev@gmail.com>
2024-01-09 00:52:49 +0100

    Create CardCountryStatisticsAtom for tracker1 and add styles

---
+++ src/components/atoms/cardCountryStatisticsTracker1/CardCountryStatisticsTracker1Atom.jsx
@@ -0,0 +1,18 @@
+import "./cardCountryStatisticsTracker1Atom.css"
+
+export const CardCountryStatisticsTracker1Atom = () => {
+    return (
+            <div className="col-md-4 col-lg-6 col-6">
+                <div className="covid_info_widget">
+                    <div className="covid_icon">
+                        <img src="./src/assets/icons/covid-blue.svg" alt="Covid Icon"/>
+                    </div>
+                    <div className="stats_info">
+                        <h4>Total cases</h4>
+                        <h3>54054</h3>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
