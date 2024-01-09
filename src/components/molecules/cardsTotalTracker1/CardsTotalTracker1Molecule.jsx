8ea824ab7d7d0a4386d56b34fec5c964b5cf6728
Desiree <desireesanchezdev@gmail.com>
2024-01-09 13:33:32 +0100

    Create CardsTotalMolecule for Tracker1, add styles and import CardTotalStatisticsAtom

---
+++ src/components/molecules/cardsTotalTracker1/CardsTotalTracker1Molecule.jsx
@@ -0,0 +1,19 @@
+import "./cardsTotalTracker1Molecule.css"
+import { CardTotalStatisticsTracker1Atom } from "../../atoms/cardTotalStatisticsTracker1/cardTotalStatisticsTracker1Atom"
+
+export const CardsTotalTracker1Molecule = () => {
+  return (
+    
+      <div className='row content_cards_total_statistics'>
+        <CardTotalStatisticsTracker1Atom></CardTotalStatisticsTracker1Atom>
+        <CardTotalStatisticsTracker1Atom></CardTotalStatisticsTracker1Atom>
+        <CardTotalStatisticsTracker1Atom></CardTotalStatisticsTracker1Atom>
+        <CardTotalStatisticsTracker1Atom></CardTotalStatisticsTracker1Atom>
+        <CardTotalStatisticsTracker1Atom></CardTotalStatisticsTracker1Atom>
+
+      </div>
+    
+
+  )
+}
+
