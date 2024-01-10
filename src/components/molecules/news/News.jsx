11826b3ae9a33b1515c66da21abc0d46c2c55e6b
martanuan <martanyp@hotmail.com>
2024-01-10 20:59:13 +0100

    Add new some more styles to home and news

--- src/components/molecules/news/News.jsx
+++ src/components/molecules/news/News.jsx
@@ -1,6 +1,6 @@
 import "./news.css";
 import useNews from "../../../hooks/useNews";
- 
+
 export const News = () => {
   const {data, loading} = useNews();
 
@@ -13,6 +13,7 @@ export const News = () => {
   };
   return (
     <section className='background-image'>
+      <h1 className="fs-6">News</h1>
       <h1>Latest News About</h1>
       <h1>Corona Virus</h1>
       <div className="card-group">
@@ -23,9 +24,9 @@ export const News = () => {
             <div key={item.key} className="card my-2 mx-2 my-card">
               <img src={`${item.image}`} style={{ marginLeft:'40px', width: '370px' }} alt="" />
               <div className="card-body" style={{ width: '370px' }}>
-                <p className='detailsContent__subtitle'><span className='mdi mdi-timer-outline'></span> {formatDate(item.newsDate)} <span className=' mdi mdi-comment-outline'></span> {item.comments} comments</p>
+                <p className='detailsContent__subtitle text-body-tertiary'><span className='mdi mdi-timer-outline'></span> {formatDate(item.newsDate)} <span className=' mdi mdi-comment-outline'></span> {item.comments} comments</p>
                 <h4><a href={item.link.url}>{item.link.text}</a></h4>
-                <p><a href={item.link.url}>Continue</a></p>
+                <p><a className="link-primary" href={item.link.url}>Continue</a></p>
               </div>
             </div>
           ))
