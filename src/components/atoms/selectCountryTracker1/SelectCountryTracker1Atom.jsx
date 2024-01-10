01f74b541cd3fbed1ac8265676f3882ff898bcab
Desiree <desireesanchezdev@gmail.com>
2024-01-11 00:38:49 +0100

    Edit some styles css and Boostrap on SelectCountry for Tracker1

--- src/components/atoms/selectCountryTracker1/SelectCountryTracker1Atom.jsx
+++ src/components/atoms/selectCountryTracker1/SelectCountryTracker1Atom.jsx
@@ -3,7 +3,7 @@ import "./selectCountryTracker1Atom.css"
 export const SelectCountryTracker1Atom = () => {
     return (
         <div className="row title_select">
-            <div className="content_select_country col-lg-3 col-md-12">
+            <div className="content_select_country col-lg-3 col-md-12 px-0">
                 <select className="form-select form-select-lg mb-3">
                     <option selected>Afghanistan</option>
                     <option value="1">Albania</option>
@@ -11,7 +11,7 @@ export const SelectCountryTracker1Atom = () => {
                     <option value="3">Three</option>
                 </select>
             </div>
-            <div className="col-lg-9 text-lg-end text-center content_date">
+            <div className="col-lg-9 text-lg-end text-center content_date pe-0">
                 <p>Updated: January 31, 2022</p>
             </div>
         </div>
