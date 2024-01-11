134f2d906b1549ca1202cbee1ecc53bd499fcaa1
Desiree <desireesanchezdev@gmail.com>
2024-01-11 23:36:18 +0100

    Edit Tracker1Organism to coordinate components related to country selection and COVID-19 statistics

--- src/components/organism/tracker1Organism/Tracker1Organism.jsx
+++ src/components/organism/tracker1Organism/Tracker1Organism.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "./tracker1Organism.css"
 import { SelectCountryTracker1Atom } from "../../atoms/selectCountryTracker1/SelectCountryTracker1Atom"
 import { CardsCountryTracker1Molecule } from "../../molecules/cardsCountryTracker1/CardsCountryTracker1Molecule"
@@ -5,14 +6,17 @@ import { CardsTotalTracker1Molecule } from "../../molecules/cardsTotalTracker1/C
 
 
 export const Tracker1Organism = () => {
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  const handleSelectCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
     <div className="container_organism_tracker1">
-      <SelectCountryTracker1Atom></SelectCountryTracker1Atom>
-      <CardsCountryTracker1Molecule></CardsCountryTracker1Molecule>
-      <CardsTotalTracker1Molecule></CardsTotalTracker1Molecule>
-
-
+      <SelectCountryTracker1Atom onSelectCountry={handleSelectCountry} />
+      <CardsCountryTracker1Molecule selectedCountry={selectedCountry} />
+      <CardsTotalTracker1Molecule />
     </div>
-
-  )
-}
+  );
+};
