fad26b4387bc673603659fd1303231bc0e9649c9
Desiree <desireesanchezdev@gmail.com>
2024-01-11 23:13:54 +0100

    Update SelectCountry atom to load options dynamically from endpoint

--- src/components/atoms/selectCountryTracker1/SelectCountryTracker1Atom.jsx
+++ src/components/atoms/selectCountryTracker1/SelectCountryTracker1Atom.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+import { useFetch } from "../../../hooks/useFetch"
 import "./selectCountryTracker1Atom.css"
 
-export const SelectCountryTracker1Atom = () => {
+
+
+export const SelectCountryTracker1Atom = ({ onSelectCountry }) => {
+
+    const data = useFetch("countries")
+    
+    const [selectedCountry, setSelectedCountry] = useState('');
+
+    const handleCountryChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedCountry(selectedValue);
+        onSelectCountry(selectedValue);
+    };
+
+    if (!data.length) {
+        return <p>Loading...</p>;
+    }
+
     return (
         <div className="row title_select">
             <div className="content_select_country col-lg-3 col-md-12 px-0">
-                <select className="form-select form-select-lg mb-3">
-                    <option selected>Afghanistan</option>
-                    <option value="1">Albania</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select
+                    className="form-select form-select-lg mb-3"
+                    value={selectedCountry}
+                    onChange={handleCountryChange}
+                >
+                    <option value="">Select a country</option>
+                    {data.map((country, index) => (
+                        <option key={index} value={country.country}>
+                            {country.country}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div className="col-lg-9 text-lg-end text-center content_date pe-0">
                 <p>Updated: January 31, 2022</p>
             </div>
         </div>
-    )
-}
+    );
+};
