fb480d1ddb6eeceb79db9cc172b71c0ada6b43a0
Desiree <desireesanchezdev@gmail.com>
2024-01-11 23:28:28 +0100

    Provide style props and images on CardCountry Atom for Tracker1

--- src/components/atoms/cardCountryStatisticsTracker1/CardCountryStatisticsTracker1Atom.jsx
+++ src/components/atoms/cardCountryStatisticsTracker1/CardCountryStatisticsTracker1Atom.jsx
@@ -1,18 +1,18 @@
 import "./cardCountryStatisticsTracker1Atom.css"
 
-export const CardCountryStatisticsTracker1Atom = () => {
+export const CardCountryStatisticsTracker1Atom = (props) => {
     return (
-            <div className="col-md-4 col-lg-6 col-6 container_covid_info">
-                <div className="covid_info_widget">
-                    <div className="covid_icon">
-                        <img src="./src/assets/icons/covid-blue.svg" alt="Covid Icon"/>
-                    </div>
-                    <div className="stats_info">
-                        <h4>Total cases</h4>
-                        <h3>54054</h3>
-                    </div>
+        <div className="col-md-4 col-lg-6 col-6 container_covid_info">
+            <div className="covid_info_widget">
+                <div className="covid_icon">
+                    <img src={`./src/assets/icons/${props.image}.svg`} alt="Covid Icon" />
+                </div>
+                <div className="stats_info">
+                    <h4>{props.title}</h4>
+                    <h3 style={props.style}>{props.value}</h3>
                 </div>
             </div>
+        </div>
     )
 }
 
