ad3452abf5641606bea4df61a0df50e0aff54423
Desiree <desireesanchezdev@gmail.com>
2024-01-11 23:29:49 +0100

    Provide style props on text and images on CardTotal Atom for Tracker1

--- src/components/atoms/cardTotalStatisticsTracker1/CardTotalStatisticsTracker1Atom.jsx
+++ src/components/atoms/cardTotalStatisticsTracker1/CardTotalStatisticsTracker1Atom.jsx
@@ -1,15 +1,15 @@
 import "./cardTotalStatisticsTracker1Atom.css"
 
-export const CardTotalStatisticsTracker1Atom = () => {
+export const CardTotalStatisticsTracker1Atom = (props) => {
     return (
         <div className="col-md-6 col-lg container_covid_total_info">
             <div className="covid_total_info_widget">
                 <div className="total_covid_icon">
-                    <img src="./src/assets/icons/covid-defult.svg" alt="Covid Icon" />
+                    <img src={`./src/assets/icons/${props.image}.svg`} alt="Covid Icon" />
                 </div>
                 <div className="total_stats_info">
-                    <h4>Total Confirmed</h4>
-                    <h3>54054</h3>
+                    <h4>{props.title}</h4>
+                    <h3>{props.value}</h3>
                 </div>
             </div>
         </div>
