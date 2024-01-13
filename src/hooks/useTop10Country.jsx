69c996dd6fc83a3afd1f8455fe96d39ca710434c
Angela <antunezangela89@gmail.com>
2024-01-13 21:23:07 +0100

    Use generic hook useFetch in tracker 0 hooks

--- src/hooks/useTop10Country.jsx
+++ src/hooks/useTop10Country.jsx
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
-import { API_URL } from "../configuration/config";
+import { useFetch } from "../hooks/useFetch"
 
 export function useTop10Country () {
-    const [top10Country, setTop10Country] = useState([]);
-
-    useEffect(() => {
-        const myEndpoint = `${API_URL}countries?sort=cases`;
-        fetch(myEndpoint)
-            .then(request => request.json())
-            .then(json => {
-                const dataTop10Country = json.slice(0,10).map(countryData => ({                    
-                    flag: countryData.countryInfo.flag,
-                    country: countryData.country,
-                    cases: countryData.cases
-                }));
-                
-                setTop10Country(dataTop10Country);
-            })
-    }, [setTop10Country]);
-
-    return top10Country;
+    const json = useFetch('countries?sort=cases');
+    const dataTop10Country = json.slice(0,10).map(countryData => (
+        {                    
+            flag: countryData.countryInfo.flag,
+            country: countryData.country,
+            cases: countryData.cases
+        }
+    ));
+    return dataTop10Country;
 }
