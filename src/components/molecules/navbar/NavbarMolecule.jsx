73a99294f8a7e92acb394a3dc95730a9a96b3981
Desiree <desireesanchezdev@gmail.com>
2024-01-13 18:08:16 +0100

    Edit Boostrap class and resize icons on Nav

--- src/components/molecules/navbar/NavbarMolecule.jsx
+++ src/components/molecules/navbar/NavbarMolecule.jsx
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom"
 export const NavbarMolecule = () => {
   return (
     <nav className="d-flex">
-            <div className="d-flex flex-column bg-body-tertiary">
+            <div className="d-flex flex-column">
                 <Link to="/" className="p-3">
                     <img className="coronavirus-img" src="./src/assets/images/coronavirus.png" width="50" height="50"></img>
                 </Link>
                 <ul className="nav nav-pills flex-column mb-auto text-center">
                     <li className="nav-item">
-                        <Link to="/tracker1" className="nav-link active py-3 border-bottom" aria-current="page">
-                            <img src="./src/assets/icons/pie-chart.svg" width="30" height="30"></img>
+                        <Link to="/tracker1" className="nav-link active" aria-current="page">
+                            <img src="./src/assets/icons/pie-chart.svg" width="24" height="24"></img>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/tracker2" className="nav-link py-3 border-bottom">
-                            <img src="./src/assets/icons/view-list.svg" width="30" height="30"></img>
+                        <Link to="/tracker2" className="nav-link">
+                            <img src="./src/assets/icons/view-list.svg" width="24" height="24"></img>
                         </Link>
                     </li>
                     <li>
-                        <Link to="/tracker3" className="nav-link py-3 border-bottom">
-                            <img src="./src/assets/icons/view-grid.svg" width="30" height="30"></img>
+                        <Link to="/tracker3" className="nav-link">
+                            <img src="./src/assets/icons/view-grid.svg" width="24" height="24"></img>
                         </Link>
                     </li>
 
                 </ul>
-                <div className="border-top">
+                <div>
                     <a href="#" className="d-flex align-items-center justify-content-center p-3">
                         <img src="./src/assets/icons/flickr-alt.svg" width="24" height="24"></img>
                     </a>
