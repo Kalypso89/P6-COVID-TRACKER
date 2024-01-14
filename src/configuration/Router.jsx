d584b6e32529832d8ef10f54630e98bafcd07932
Desiree <desireesanchezdev@gmail.com>
2024-01-14 22:15:00 +0100

    Rename element nav Trackers, and delete not used images

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -3,7 +3,6 @@ import { HomePage } from '../components/pages/homePage/HomePage';
 import { Tracker1Page } from '../components/pages/tracker1/Tracker1Page';
 import { Tracker2Page } from '../components/pages/tracker2/Tracker2Page';
 import { Tracker3Page } from '../components/pages/tracker3/Tracker3Page';
-import { Tracker4Page } from '../components/pages/tracker4/Tracker4Page';
 
 const Router = () => {
   return (
@@ -13,7 +12,6 @@ const Router = () => {
                 <Route path='/tracker1' element={<Tracker1Page/>}></Route>
                 <Route path='/tracker2' element={<Tracker2Page/>}></Route>
                 <Route path='/tracker3' element={<Tracker3Page/>}></Route>
-                <Route path='/tracker4' element={<Tracker4Page/>}></Route>
         </Routes>
     </BrowserRouter>
   )
